6c8e96af3631cd7e056ef9c10153774261aee683
Sylvester <nnaemekasilver@gmail.com>
2021-06-21 14:10:21 +0100

    Add portfolio.js file

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+const main = document.querySelector('main');
+const hamburger = document.querySelector('#hamburger');
+const close = document.querySelector('#close');
+const portfolio = document.querySelector('#portfolio');
+const about = document.querySelector('#about');
+const contact = document.querySelector('#contact');
