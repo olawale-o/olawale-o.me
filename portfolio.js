af3cb0f47b810bc05293bd48a480bcc7095caa87
olawale-o <omoogunolawale@gmail.com>
2021-06-21 14:30:18 +0100

    Add Click Event on hamburger close portfolio about and contact Selectors to Toggle active Class on main Selector

--- portfolio.js
+++ portfolio.js
@@ -4,3 +4,23 @@ const close = document.querySelector('#close');
 const portfolio = document.querySelector('#portfolio');
 const about = document.querySelector('#about');
 const contact = document.querySelector('#contact');
+
+hamburger.addEventListener('click', () => {
+  main.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+  main.classList.remove('active');
+});
+
+portfolio.addEventListener('click', () => {
+  main.classList.toggle('active');
+});
+
+about.addEventListener('click', () => {
+  main.classList.toggle('active');
+});
+
+contact.addEventListener('click', () => {
+  main.classList.toggle('active');
+});
